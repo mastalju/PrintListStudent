c990cd0aaf232fbffa0fa5c25b4d4f05751ab2fd
mastalju <mastalju@sheridancollege.ca>
2024-09-24 15:16:56 -0400

    student list version 1

---
+++ src/printstudentlist/PrintStudentList.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printstudentlist;
+
+/**
+ *
+ * @author ggg
+ */
+public class PrintStudentList {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Peter");
+        s1.setId(1);
+        
+        Student s2 = new Student();
+        s2.setName("John");
+        s2.setId(2);
+        
+        Student s3 = new Student();
+        s3.setName("Jess");
+        s3.setId(3);
+        
+        Student [] list = new Student [3];
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3;
+        
+        for(int i=0;i<list.length;i++){
+            System.out.println(list[i].getName() + " " + list[i].getId());
+        }
+    }
+    
+}
